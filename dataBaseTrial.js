227f000c0c644e6719fbc8a5fed4a493e29d81d5
nt554 <nt554@drexel.edu>
2021-04-17 14:02:53 -0400

    Adding Trail DataBase

---
+++ dataBaseTrial.js
@@ -0,0 +1,95 @@
+dataBaseJSON={
+    medicalConditions:[
+        {
+            chronicDiseases:[
+                {
+                    name:"HeartAttack",
+                    firstEncounterYear:2010,
+                    currentMedicine:"KandoKoJhol",
+                    totalCTScans:4,
+                    totalMRIs:2,
+                    lastCTScanDate:"Nov 19,2020",
+                    lastMRI:"Oct 23, 2021",
+                    CTMRILinks:"https://google.com/",
+                    medicalReports:[
+                        {
+                            date:2020,
+                            hospital:"Yasodha Hospital",
+                            doctorName:"Dr. hari Om",
+                            reportLink:"https://google.com/",
+                            otherAttachments:[
+                                {
+                                    name:"Bill Invoice",
+                                    link:"https://google.com"
+                                }
+                            ]
+    
+                        }
+                    ]
+                }
+            ],
+            allergies:[
+                {
+                    name:"Pollen Allergies",
+                    attachmeths:"https://google.com"
+                }
+            ],
+    bloodGroup:bloodGroup,
+    seriousInjuries:[
+        {
+            typeOfInjuries:"Head Injury",
+            timeOfInjury:"Nov 19, 2019"
+        }
+    ],
+    vaccination:[
+        {
+            nameOfVaccination:"Moderna",
+            timeTaken:"14 Apr, 2021"
+        }
+    ]
+        }
+    ],
+    
+    emergencyContacts:[
+        {
+            name:"Tara Prasad Oli",
+            relationship:"Father",
+            phoneNumber:9856785678,
+            address:"Gairegau Butwal"
+        },
+        {
+            name:"Tara Prasad Oli",
+            relationship:"Father",
+            phoneNumber:9856785678,
+            address:"Gairegau Butwal"
+        },
+        {
+            name:"Tara Prasad Oli",
+            relationship:"Father",
+            phoneNumber:9856785678,
+            address:"Gairegau Butwal"
+        },
+        {
+            name:"Tara Prasad Oli",
+            relationship:"Father",
+            phoneNumber:9856785678,
+            address:"Gairegau Butwal"
+        }
+    ],
+    userInfo:[
+        {
+            name:"Nishan",
+            email:"nishan@ishan.com",
+            userProfile:"https://upload.wikimedia.org/wikipedia/commons/3/3b/Johnny_Depp-2757_%28cropped%29.jpg",
+            bloodGroup:bloodGroup,
+            address:"Ghorahi Hari Om Prasad Don",
+            emergencyNotes:[
+                {
+                    title:"Take me to nearest Neuro Hospital",
+                    description:"aksjfbn sdfskdhfnbksd fsdlkghdskf dslkgbdskjgb",
+                    whatToDoNow:"sfnsdkj sdkjgbsdkg sdkjgb"
+                }
+            ]
+        }
+    ]
+}
